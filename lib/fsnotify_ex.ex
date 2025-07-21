0e4d3e2b20653bc85859e009ce029ceccf237dd0
Jessy Williams <jessy@jessywilliams.com>
2025-07-20 23:38:33 -0400

    Initial commit

---
+++ lib/fsnotify_ex.ex
@@ -0,0 +1,18 @@
+defmodule FsnotifyEx do
+  @moduledoc """
+  Documentation for `FsnotifyEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FsnotifyEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fsnotify_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule FsnotifyExTest do
+  use ExUnit.Case
+  doctest FsnotifyEx
+
+  test "greets the world" do
+    assert FsnotifyEx.hello() == :world
+  end
+end
